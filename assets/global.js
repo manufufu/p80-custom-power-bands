97ec2ec6c324fab1e08de7946c9cc501e48bafa6
manufufu <aldrinespinosa910@yahoo.com>
2023-05-03 19:03:23 +0800

    add: Shopify format money

--- assets/global.js
+++ assets/global.js
@@ -283,6 +283,52 @@ Shopify.CountryProvinceSelector.prototype = {
   }
 };
 
+Shopify.money_format = "${{amount}}";
+
+Shopify.formatMoney = function(cents, format) {
+  if (typeof cents === 'string') { cents = cents.replace('.', ''); }
+  var value = '';
+  var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
+  var formatString = (format || this.money_format);
+
+  function defaultOption(opt, def) {
+     return opt ?? def;
+  }
+
+  function formatWithDelimiters(number, precision, thousands, decimal) {
+    precision = defaultOption(precision, 2);
+    thousands = defaultOption(thousands, ',');
+    decimal   = defaultOption(decimal, '.');
+
+    if (isNaN(number) || number == null) { return 0; }
+
+    number = +(number / 100).toFixed(precision);
+
+    var parts   = number.toString().split('.'),
+        dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+        cents   = parts[1] ? (decimal + parts[1]) : '';
+
+    return dollars + cents;
+  }
+
+  switch(formatString.match(placeholderRegex)[1]) {
+    case 'amount':
+      value = formatWithDelimiters(cents, 2);
+      break;
+    case 'amount_no_decimals':
+      value = formatWithDelimiters(cents, 0);
+      break;
+    case 'amount_with_comma_separator':
+      value = formatWithDelimiters(cents, 2, '.', ',');
+      break;
+    case 'amount_no_decimals_with_comma_separator':
+      value = formatWithDelimiters(cents, 0, '.', ',');
+      break;
+  }
+
+  return formatString.replace(placeholderRegex, value);
+};
+
 class MenuDrawer extends HTMLElement {
   constructor() {
     super();
@@ -752,6 +798,7 @@ customElements.define('slideshow-component', SlideshowComponent);
 class VariantSelects extends HTMLElement {
   constructor() {
     super();
+    this.metafieldVariantPrice = [];
     this.addEventListener('change', this.onVariantChange);
   }
 
@@ -778,7 +825,6 @@ class VariantSelects extends HTMLElement {
 
   updateOptions() {
     this.options = Array.from(this.querySelectorAll('select'), (select) => select.value);
-    console.log(this.options);
   }
 
   updateMasterId() {
@@ -903,13 +949,24 @@ class VariantSelects extends HTMLElement {
     )
       .then((response) => response.text())
       .then((responseText) => {
+        if(this.currentVariant.options.length !== this.options){
+          const filterMetafields = this.options.filter(option => !this.currentVariant.options.includes(option));
+          const metafieldPrice = [];
+          this.getVariantData().map(variant => {
+            filterMetafields.forEach(metaVariant => {
+              if(metaVariant == variant.option1){
+                metafieldPrice.push(variant.price);
+              }
+            });
+          });
+          console.log(metafieldPrice);
+        }
         const html = new DOMParser().parseFromString(responseText, 'text/html');
         const destination = document.getElementById(`price-${this.dataset.section}`);
         const source = html.getElementById(
           `price-${this.dataset.originalSection ? this.dataset.originalSection : this.dataset.section}`
         );
         if (source && destination) destination.innerHTML = source.innerHTML;
-
         const price = document.getElementById(`price-${this.dataset.section}`);
 
         if (price) price.classList.remove('visibility-hidden');
